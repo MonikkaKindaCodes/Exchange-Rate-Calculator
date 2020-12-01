6a5959cafd20d86cc676a34c7468eb307197692f
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-11-30 23:18:56 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
-
 const currencyElementOne = document.getElementById('currency-one');
 const amountElementOne = document.getElementById('amount-one');
 const currencyElementTwo = document.getElementById('currency-two');
 const amountElementTwo = document.getElementById('amount-two');
-const rateElement = document.getElementById('rate');
+
+const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
-//Fetch Exchange Rates and Updates the DOM
+
+//Fetch Exchange Rate and Update DOM
 function calculate () {
-   const currency_one = currencyElementOne.value;
-   const currency_two = currencyElementOne.value;
+const currency_one = currencyElementOne.value;
+const currency_two = currencyElementTwo.value;
 
-   console.log(currency_one, currency_two);
+fetch(`https://v6.exchangerate-api.com/v6/latest/${currency_one}`)
+.then(res => res.json())
+.then(data => {
+ const rate = data.rates[currency_two];
+
+ rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two} `;
+
+ amountElementTwo.value = (amountElementOne.value * rate).toFixed(2);
+});
 }
-
-
 
 //Event Listeners
 currencyElementOne.addEventListener('change', calculate);
@@ -22,4 +29,10 @@ amountElementOne.addEventListener('input', calculate);
 currencyElementTwo.addEventListener('change', calculate);
 amountElementTwo.addEventListener('input', calculate);
 
+swap.addEventListener('click', () => {
+    const temp = currencyElementOne.value;
+    currencyElementOne.value = currencyElementTwo.value;
+    currencyElementTwo.value = temp; 
+    calculate();
+})
 calculate();
